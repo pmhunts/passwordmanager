0ea9174e728f3232fd2472063073e629c9085b9e
pmhunts <2k22.csai.2213601@gmail.com>
2025-04-24 19:17:36 +0530

    mishra
    mishra

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ const strengthText = document.getElementById('strengthText');
 const strengthIndicator = document.getElementById('strengthIndicator');
 
 // API Base URL - change this to your server URL in production
-const API_BASE_URL = 'https://passwordmanager-9rsw.onrender.com';
+const API_BASE_URL = 'https://passwordmanager-9rsw.onrender.com/api';
 
 // State variables
 let isLogin = true;
